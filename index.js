db2ed8ac5a9bea3363ca80d612d0db7fd2451529
thzero <thzero@users.noreply.github.com>
2020-09-27 08:46:19 -0500

    correlationId updates

--- index.js
+++ index.js
@@ -23,78 +23,80 @@ class LoggerService extends Service {
 		}
 	}
 
-	debug(clazz, method, message, data, isClient) {
+	debug(clazz, method, message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.debug(data, this._format(clazz, method, message, isClient));
+		this._log.debug(data, this._format(clazz, method, message, correlationId, isClient));
 	}
 
-	debug2(message, data, isClient) {
+	debug2(message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.debug(data, this._format(null, null, message, isClient));
+		this._log.debug(data, this._format(null, null, message, correlationId, isClient));
 	}
 
-	error(clazz, method, message, data, isClient) {
+	error(clazz, method, message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.error(data, this._format(clazz, method, message, isClient));
+		this._log.error(data, this._format(clazz, method, message, correlationId, isClient));
 	}
 
-	error2(message, data, isClient) {
+	error2(message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.error(data, this._format(null, null, message, isClient));
+		this._log.error(data, this._format(null, null, message, correlationId, isClient));
 	}
 
-	exception(clazz, method, ex, isClient) {
+	exception(clazz, method, ex, correlationId, isClient) {
 		ex = (ex === undefined ? null : ex);
-		this._log.error(ex, this._format(clazz, method, null, isClient));
+		this._log.error(ex, this._format(clazz, method, null, correlationId, isClient));
 	}
 
-	exception2(ex, isClient) {
+	exception2(ex, correlationId, isClient) {
 		ex = (ex === undefined ? null : ex);
-		this._log.error(ex, this._format(null, null, null, isClient));
+		this._log.error(ex, this._format(null, null, null, correlationId, isClient));
 	}
 
-	fatal(clazz, method, message, data, isClient) {
+	fatal(clazz, method, message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.fatal(data, this._format(clazz, method, message, isClient));
+		this._log.fatal(data, this._format(clazz, method, message, correlationId, isClient));
 	}
 
-	fatal2(message, data, isClient) {
+	fatal2(message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.fatal(data, this._format(null, null, message, isClient));
+		this._log.fatal(data, this._format(null, null, message, correlationId, isClient));
 	}
 
-	info(clazz, method, message, data, isClient) {
+	info(clazz, method, message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.info(data, this._format(clazz, method, message, isClient));
+		this._log.info(data, this._format(clazz, method, message, correlationId, isClient));
 	}
 
-	info2(message, data, isClient) {
+	info2(message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.info(data, this._format(null, null, message, isClient));
+		this._log.info(data, this._format(null, null, message, correlationId, isClient));
 	}
 
-	trace(clazz, method, message, data, isClient) {
+	trace(clazz, method, message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.trace(data, this._format(clazz, method, message, isClient));
+		this._log.trace(data, this._format(clazz, method, message, correlationId, isClient));
 	}
 
-	trace2(message, data, isClient) {
+	trace2(message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.trace(data, this._format(null, null, message, isClient));
+		this._log.trace(data, this._format(null, null, message, correlationId, isClient));
 	}
 
-	warn(clazz, method, message, data, isClient) {
+	warn(clazz, method, message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.warn(data, this._format(clazz, method, message, isClient));
+		this._log.warn(data, this._format(clazz, method, message, correlationId, isClient));
 	}
 
-	warn2(message, data, isClient) {
+	warn2(message, data, correlationId, isClient) {
 		data = (data === undefined ? null : data);
-		this._log.warn(data, this._format(null, null, message, isClient));
+		this._log.warn(data, this._format(null, null, message, correlationId, isClient));
 	}
 
-	_format(clazz, method, message, isClient) {
+	_format(clazz, method, message, correlationId, isClient) {
 		let output = '';
+		if (!String.isNullOrEmpty(correlationId))
+			output += `(${correlationId}) `;
 		if (!String.isNullOrEmpty(clazz))
 			output += clazz;
 		if (!String.isNullOrEmpty(output))
