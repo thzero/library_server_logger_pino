1ffaa8dabfbbcf050f62004c5a979f5bd68365f5
thzero <thzero@users.noreply.github.com>
2022-12-20 07:48:23 -0600

    updates for opensource

---
+++ openSource.js
@@ -0,0 +1,39 @@
+export default () => {
+    return [
+        {
+            category: 'server',
+            name: '@thzero/library_common',
+            url: 'https://github.com/thzero/library_common',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: '@thzero/library_common_service',
+            url: 'https://github.com/thzero/library_common_service',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common_service/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: '@thzero/library_service',
+            url: 'https://github.com/thzero/library_service',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_service/blob/master/license.md'
+        },
+		{
+			category: 'server',
+			name: '@thzero/library_server_logger_pino',
+			url: 'https://github.com/thzero/library_server_logger_pino',
+			licenseName: 'MIT',
+			licenseUrl: 'https://github.com/thzero/library_server_logger_pino/blob/master/license.md'
+		},
+		{
+			category: 'server',
+			name: 'pino',
+			url: 'https://github.com/pinojs/pino',
+			licenseName: 'MIT',
+			licenseUrl: 'https://github.com/pinojs/pino/blob/master/LICENSE'
+		}
+    ];
+}
